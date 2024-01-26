e5dec17380333ce60148028dc12d17c0d3c72929
rahulk79 <rahul.kashyap7sep@gmail.com>
2024-01-26 23:16:15 +0530

    removed dead code

--- src/test/java/SauceLabDemoAutomation/testSauseLabDemoLogin.java
+++ src/test/java/SauceLabDemoAutomation/testSauseLabDemoLogin.java
@@ -28,29 +28,7 @@ public class testSauseLabDemoLogin {
 	@BeforeTest
 	public void setpage() throws IOException {
 
-		//DesiredCapabilities capabilities = new DesiredCapabilities();
-
-       // capabilities.setCapability("os", "Windows");
-       // capabilities.setCapability("osVersion", "10");
-       // capabilities.setCapability("browser", "Chrome");
-       // capabilities.setCapability("browser_Version", "80");
-       // capabilities.setCapability("name", "BS_DEMO");
-       
-        
-       // HashMap<String, Object> chromeOptions = new HashMap<String, Object>();
-       // chromeOptions.put("driver", "2.45");
-       // HashMap<String, Object> browserstackOptions = new HashMap<String, Object>();
-       // browserstackOptions.put("os", "Windows");
-       // browserstackOptions.put("osVersion", "11");
-       // browserstackOptions.put("projectName", "BS_DEMO");
-       // browserstackOptions.put("networkLogs", "true");
-       // browserstackOptions.put("browserstack", browserstackOptions);
-       //  browserstackOptions.put("chrome", chromeOptions);
-       // capabilities.setCapability("bstack:options", browserstackOptions);
-       // capabilities.setCapability("browserName", "Chrome");
-       // capabilities.setCapability("browserVersion", "103.0");
-
-       MutableCapabilities capabilities = new MutableCapabilities();
+	   MutableCapabilities capabilities = new MutableCapabilities();
        HashMap<String, Object> bstackOptions = new HashMap<String, Object>();
        capabilities.setCapability("browserName", "Chrome");
        bstackOptions.put("os", "Windows");
@@ -60,8 +38,6 @@ public class testSauseLabDemoLogin {
        bstackOptions.put("resolution", "1024x768");
        capabilities.setCapability("bstack:options", bstackOptions);
        driver = new RemoteWebDriver(new URL(URL), capabilities);
-       
-    
     }
   
 	@Test(description="A user can login successfully",priority=1)
