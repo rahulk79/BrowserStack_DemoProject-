fd080a6571eb5311998b3894f7464a3da3775a52
rahulk79 <rahul.kashyap7sep@gmail.com>
2024-01-26 18:15:42 +0530

    code updated

--- src/test/java/SauceLabDemoAutomation/testSauseLabDemoLogin.java
+++ src/test/java/SauceLabDemoAutomation/testSauseLabDemoLogin.java
@@ -1,38 +1,57 @@
-package MetribesAutomation.MetribesAutomation;
+package SauceLabDemoAutomation;
 
 import java.io.IOException;
-import org.testng.Assert;
+import java.util.HashMap;
+
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import mtribesPageLibrary.mtribesQASandboxPage;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
+
 import userActions.userAction;
-import utility.fileUtil;
 
 
-public class testMtribesApp {
+
+public class testSauseLabDemoLogin {
 	public static WebDriver driver ;
+
+	public static final String userName = "";
+	public static final String accessKey ="";
+	public static final String URL = "https://" + userName +":" + accessKey+ "@hub-cloud.browserstack.com/wd/hub ";
 	//public static Properties prop ;
 	//public static WebDriverWait webwait ;
 	
 	
 	@BeforeTest
 	public void setpage() throws IOException {
-		WebDriverManager.chromedriver().setup();
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "Chrome");
+        capabilities.setCapability("browserVersion", "103.0");
+        HashMap<String, Object> browserstackOptions = new HashMap<String, Object>();
+        browserstackOptions.put("os", "Windows");
+        browserstackOptions.put("osVersion", "11");
+        browserstackOptions.put("projectName", "BS_DEMO");
+        browserstackOptions.put("networkLogs", "true");
+        browserstackOptions.put("browserstack", browserstackOptions);
+        HashMap<String, Object> chromeOptions = new HashMap<String, Object>();
+        chromeOptions.put("driver", "2.45");
+        browserstackOptions.put("chrome", chromeOptions);
+        capabilities.setCapability("bstack:options", browserstackOptions);
+
+		driver = new RemoteWebDriver(new URL(URL), capabilities);
+
+		
 		
 		}
   
-	@Test(description="A user can login and logout of mtribes successfully",priority=1)
-	public void verifyLoginAndLogout() throws IOException {
+	@Test(description="A user can login successfully",priority=1)
+	public void verifyLogin() throws IOException {
 		driver = new ChromeDriver();
 		userAction.perfromLogin(driver);
-		Assert.assertTrue(mtribesQASandboxPage.Welcome_Message(driver).getText()
-			  .contains(fileUtil.readProperty("user_fullname")), "User login is failing ");
-		userAction.performLogout(driver);
-		Assert.assertTrue(driver.getTitle().contains("Real-time UX control"), "User logout is failing");
 		}
 	
   @AfterTest
